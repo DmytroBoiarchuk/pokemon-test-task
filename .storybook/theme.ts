a987f72d0815169a37ee13a152c7962465afcb63
DmytroBoiarchuk <dimabwhite98@gmail.com>
2025-03-13 21:32:15 +0000

    stirybook build fix 2

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -3,5 +3,5 @@ import { create } from "@storybook/theming";
 export default create({
   base: "dark",
   brandTitle: "Luna Edge",
-  brandImage: 'public/LunaEdgeLogo.svg',
+  brandImage: '/LunaEdgeLogo.svg',
 });
